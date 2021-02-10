347b5973080707b4387035aeb12928cb169ffc4b
ziemas <ziemas@ziemas.se>
2021-02-10 23:51:32 +0000

    SPU2: WIP building WX conf on windows

--- pcsx2/SPU2/SndOut_XAudio2.cpp
+++ pcsx2/SPU2/SndOut_XAudio2.cpp
@@ -15,7 +15,7 @@
 
 #include "PrecompiledHeader.h"
 #include "SPU2/Global.h"
-#include "Dialogs.h"
+#include "wx/Dialogs.h"
 
 #include <xaudio2.h>
 #include <cguid.h>
@@ -91,7 +91,9 @@ private:
 
 		BaseStreamingVoice(uint numChannels)
 			: pSourceVoice(nullptr)
-			, m_nBuffers(Config_XAudio2.NumBuffers)
+			// TODO
+			//, m_nBuffers(Config_XAudio2.NumBuffers)
+			, m_nBuffers(2)
 			, m_nChannels(numChannels)
 			, m_BufferSize(SndOutPacketSize * m_nChannels * PacketsPerBuffer)
 			, m_BufferSizeBytes(m_BufferSize * sizeof(s16))
--- pcsx2/SPU2/wx/wxConfig.cpp
+++ pcsx2/SPU2/wx/wxConfig.cpp
@@ -14,11 +14,9 @@
  */
 
 #include "PrecompiledHeader.h"
-#include "SPU2/Config.h"
-#if defined(__linux__) || defined(__APPLE__)
-#include "SPU2/wx/Config.h"
-#endif
-#include "SPU2/Global.h"
+#include "../Config.h"
+#include "../wx/Config.h"
+#include "../Global.h"
 #include "wxConfig.h"
 
 MixerTab::MixerTab(wxWindow* parent)
@@ -352,10 +350,15 @@ Dialog::Dialog()
 
 	wxArrayString module_entries;
 	module_entries.Add("No Sound (Emulate SPU2 only)");
+#ifdef _WIN32
+	module_entries.Add("XAudio");
+#endif
 #ifdef SPU2X_PORTAUDIO
 	module_entries.Add("PortAudio (Cross-platform)");
 #endif
+#ifndef _WIN32
 	module_entries.Add("SDL Audio (Recommended for PulseAudio)");
+#endif
 	m_module_select = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, module_entries);
 	module_box->Add(m_module_select, wxSizerFlags().Centre());
 
@@ -372,6 +375,8 @@ Dialog::Dialog()
 	portaudio_entries.Add("JACK");
 #elif defined(__APPLE__)
 	portaudio_entries.Add("CoreAudio");
+#elif defined(_WIN32)
+	portaudio_entries.Add("?"); // TODO
 #else
 	portaudio_entries.Add("OSS");
 #endif
@@ -379,6 +384,7 @@ Dialog::Dialog()
 	m_portaudio_box->Add(m_portaudio_select, wxSizerFlags().Centre());
 #endif
 
+#ifndef _WIN32
 	// SDL
 	m_sdl_box = new wxBoxSizer(wxVERTICAL);
 	m_sdl_text = new wxStaticText(this, wxID_ANY, "SDL API");
@@ -390,11 +396,12 @@ Dialog::Dialog()
 
 	m_sdl_select = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, sdl_entries);
 	m_sdl_box->Add(m_sdl_select, wxSizerFlags().Centre());
+	module_box->Add(m_sdl_box, wxSizerFlags().Expand());
+#endif
 
 #ifdef SPU2X_PORTAUDIO
 	module_box->Add(m_portaudio_box, wxSizerFlags().Expand());
 #endif
-	module_box->Add(m_sdl_box, wxSizerFlags().Expand());
 
 	m_top_box->Add(module_box, wxSizerFlags().Centre().Border(wxALL, 5));
 
@@ -468,7 +475,9 @@ void Dialog::Load()
 #ifdef SPU2X_PORTAUDIO
 	m_portaudio_select->SetSelection(OutputAPI);
 #endif
+#ifndef _WIN32
 	m_sdl_select->SetSelection(SdlOutputAPI);
+#endif
 
 	m_mixer_panel->Load();
 	m_sync_panel->Load();
@@ -490,9 +499,10 @@ void Dialog::Save()
 		p_api = "OSS";
 	PortaudioOut->SetApiSettings(p_api);
 #endif
-
+#ifndef _WIN32
 	SdlOutputAPI = m_sdl_select->GetSelection();
 	SDLOut->SetApiSettings(m_sdl_select->GetStringSelection());
+#endif
 
 	m_mixer_panel->Save();
 	m_sync_panel->Save();
