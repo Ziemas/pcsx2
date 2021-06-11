4c339ccad91a6f1b8f792fc0c238595180ad731c
Ziemas <ziemas@ziemas.se>
2021-06-11 09:48:25 +0200

    SPU: Constexpr interpolation table
    Using gcem to replace the cmath calls in the original implementation.
    
    Verified that this produces an accurate table.

--- pcsx2/SPU2/Voice.cpp
+++ pcsx2/SPU2/Voice.cpp
@@ -14,12 +14,50 @@
  */
 
 #include "SpuCore.h"
+#include "gcem.hpp"
 #include "common/Console.h"
 #include "Voice.h"
 
 namespace SPU
 {
 	class SPUCore;
+	static constexpr std::array<std::array<s16, 4>, 256> gaussianConstructTable()
+	{
+		std::array<std::array<s16, 4>, 256> result = {};
+		double table[512] = {};
+		for (u32 n = 0; n < 512; n++)
+		{
+			double k = 0.5 + n;
+			double s = (gcem::sin(GCEM_PI * k * 2.048 / 1024));
+			double t = (gcem::cos(GCEM_PI * k * 2.000 / 1023) - 1) * 0.50;
+			double u = (gcem::cos(GCEM_PI * k * 4.000 / 1023) - 1) * 0.08;
+			double r = s * (t + u + 1.0) / k;
+			table[511 - n] = r;
+		}
+		double sum = 0.0;
+		for (u32 n = 0; n < 512; n++)
+			sum += table[n];
+		double scale = 0x7f80 * 128 / sum;
+		for (u32 n = 0; n < 512; n++)
+			table[n] *= scale;
+		for (u32 phase = 0; phase < 256; phase++)
+		{
+			double sum = 0.0;
+			sum += table[phase + 0];
+			sum += table[phase + 256];
+			sum += table[511 - phase];
+			sum += table[255 - phase];
+			double diff = (sum - 0x7f80) / 4;
+			result[255 - phase][0] = gcem::round(table[phase + 0] - diff);
+			result[255 - phase][1] = gcem::round(table[phase + 256] - diff);
+			result[255 - phase][2] = gcem::round(table[511 - phase] - diff);
+			result[255 - phase][3] = gcem::round(table[255 - phase] - diff);
+		}
+
+		return result;
+	}
+
+	static constexpr std::array<std::array<s16, 4>, 256> gaussianTable = gaussianConstructTable();
 
 	s16 Voice::GenSample()
 	{
