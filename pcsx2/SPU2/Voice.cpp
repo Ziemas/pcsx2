5755c6cb884fa12b4decebe8073f3d61ce87cdfc
Ziemas <ziemas@ziemas.se>
2021-06-11 09:52:16 +0200

    SPU: Start fleshing out voice logic

--- pcsx2/SPU2/Voice.cpp
+++ pcsx2/SPU2/Voice.cpp
@@ -20,7 +20,15 @@
 
 namespace SPU
 {
-	class SPUCore;
+	// Integer math version of ps-adpcm coefs
+	static constexpr s32 adpcm_coefs_i[5][2] = {
+		{0, 0},
+		{60, 0},
+		{115, -52},
+		{98, -55},
+		{122, -60},
+	};
+
 	static constexpr std::array<std::array<s16, 4>, 256> gaussianConstructTable()
 	{
 		std::array<std::array<s16, 4>, 256> result = {};
@@ -59,8 +67,102 @@ namespace SPU
 
 	static constexpr std::array<std::array<s16, 4>, 256> gaussianTable = gaussianConstructTable();
 
+	void Voice::DecodeSamples()
+	{
+		if (m_DecodeBuf.Size() > 11)
+		{
+			// sufficient data buffered
+			return;
+		}
+
+		// full block decode
+		//  shift  = 12 - (src[4+blk*2+nibble] AND 0Fh)
+		//  filter =      (src[4+blk*2+nibble] AND 30h) SHR 4
+		//  f0 = pos_xa_adpcm_table[filter]
+		//  f1 = neg_xa_adpcm_table[filter]
+		//  for j=0 to 27
+		//    t = signed4bit((src[16+blk+j*4] SHR (nibble*4)) AND 0Fh)
+		//    s = (t SHL shift) + ((old*f0 + older*f1+32)/64);
+		//    s = MinMax(s,-8000h,+7FFFh)
+		//    halfword[dst]=s, dst=dst+2, older=old, old=s
+		//  next j
+
+		if ((m_NAX.full & 0x7) == 0)
+		{
+			if (m_CurHeader.LoopEnd)
+			{
+				m_NAX.full = m_LSA.full;
+				m_ENDX = true;
+
+				if (!m_CurHeader.LoopRepeat)
+				{
+					//release adsr and set to 0
+				}
+			}
+
+			m_CurHeader.bits = m_SPU.Ram(m_NAX.full & ~0x7);
+			if (m_CurHeader.LoopStart)
+				m_LSA.full = m_NAX.full & ~0x7;
+		}
+	}
+
 	s16 Voice::GenSample()
 	{
+		if (m_KeyOff)
+		{
+			m_KeyOff = false;
+			// TODO release env
+			m_ENVX = 0;
+			Console.WriteLn("SPU[%d]:VOICE[%d] Key Off", m_SPU.m_Id, m_Id);
+		}
+		if (m_KeyOn)
+		{
+			m_KeyOn = false;
+			// TODO env attack
+			m_ENVX = 0x7fff;
+			m_NAX.full = m_SSA.full;
+			m_LSA.full = m_SSA.full;
+			m_Counter = 0;
+			m_DecodeBuf.Reset();
+			Console.WriteLn("SPU[%d]:VOICE[%d] Key On, SSA %08x", m_SPU.m_Id, m_Id, m_SSA);
+		}
+
+		DecodeSamples();
+
+		// Interpolation:
+		//   nocash describes interpolation as happening to the 4 most recent samples
+		//   do we start with the first sample and 0 for the 3 previous?
+		//   or do we decode 4 samples and interpolate using the first 4?
+		//   mednafen does the latter, and i could see why that might make sense
+
+		u32 sample = 0;
+		u32 index = (m_Counter & 0x0FF0) >> 4;
+		sample += (m_DecodeBuf.Peek(0) * gaussianTable[index][0]) >> 15;
+		sample += (m_DecodeBuf.Peek(1) * gaussianTable[index][1]) >> 15;
+		sample += (m_DecodeBuf.Peek(2) * gaussianTable[index][2]) >> 15;
+		sample += (m_DecodeBuf.Peek(3) * gaussianTable[index][3]) >> 15;
+
+		// Step = VxPitch                  ;range +0000h..+FFFFh (0...705.6 kHz)
+		//  IF PMON.Bit(x)=1 AND (x>0)      ;pitch modulation enable
+		//    Factor = VxOUTX(x-1)          ;range -8000h..+7FFFh (prev voice amplitude)
+		//    Factor = Factor+8000h         ;range +0000h..+FFFFh (factor = 0.00 .. 1.99)
+		//    Step=SignExpand16to32(Step)   ;hardware glitch on VxPitch>7FFFh, make sign
+		//    Step = (Step * Factor) SAR 15 ;range 0..1FFFFh (glitchy if VxPitch>7FFFh)
+		//    Step=Step AND 0000FFFFh       ;hardware glitch on VxPitch>7FFFh, kill sign
+		//  IF Step>3FFFh then Step=4000h   ;range +0000h..+3FFFh (0.. 176.4kHz)
+		//  Counter = Counter + Step
+
+		u32 step = m_Pitch;
+		// TODO pitch mod
+		step = std::min(step, 0x3FFFu);
+		m_Counter += step;
+
+		while (m_Counter >= 0x1000u)
+		{
+			m_Counter -= 0x1000u;
+			m_DecodeBuf.Pop();
+		}
+
 		return 0;
 	}
 
