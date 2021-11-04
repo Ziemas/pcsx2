a3ba56139c5c5cd7dfce4e6320060a72bca5b5b8
Ziemas <ziemas@ziemas.se>
2021-11-04 20:54:55 +0100

    SPU: Unconditinally read ADMA input buffer
    Should fix burnout

--- pcsx2/SPU2/SpuCore.cpp
+++ pcsx2/SPU2/SpuCore.cpp
@@ -60,15 +60,12 @@ namespace SPU
 
 		AudioSample MemIn(0, 0);
 
-		if (AdmaActive())
-		{
-			auto displacement = (m_CurrentBuffer * BufSize) + m_BufPos + (m_Id * InBufOffset);
-			auto laddress = static_cast<u32>(InBuf::MeminL) + displacement;
-			auto raddress = static_cast<u32>(InBuf::MeminR) + displacement;
-			MemIn.left = static_cast<s16>(m_RAM[laddress]);
-			MemIn.right = static_cast<s16>(m_RAM[raddress]);
-			MemIn.Volume(m_BVOL);
-		}
+		auto displacement = (m_CurrentBuffer * BufSize) + m_BufPos + (m_Id * InBufOffset);
+		auto laddress = static_cast<u32>(InBuf::MeminL) + displacement;
+		auto raddress = static_cast<u32>(InBuf::MeminR) + displacement;
+		MemIn.left = static_cast<s16>(m_RAM[laddress]);
+		MemIn.right = static_cast<s16>(m_RAM[raddress]);
+		MemIn.Volume(m_BVOL);
 
 		Dry.Mix(MemIn, m_MMIX.MeminL, m_MMIX.MeminR);
 		Wet.Mix(MemIn, m_MMIX.MeminWetL, m_MMIX.MeminWetR);
