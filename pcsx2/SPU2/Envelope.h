7b5718bdd93b2c21520e0c3c1c795d42422aa691
Ziemas <ziemas@ziemas.se>
2022-11-02 06:03:00 +0100

    move envelope into header for inlining

--- pcsx2/SPU2/Envelope.h
+++ pcsx2/SPU2/Envelope.h
@@ -57,7 +57,37 @@ namespace SPU
 	class Envelope
 	{
 	public:
-		void Step();
+		void Step()
+		{
+			// arbitrary number of bits, this is probably incorrect for the
+			// "reserved" and infinite duration values
+			// test hw or copy mednafen instead?
+
+			u32 cStep = 0x8000;
+
+			s32 shift = m_Shift - 11;
+			if (shift > 0)
+				cStep >>= shift;
+
+			s16 step = static_cast<s16>(m_Step << std::max(0, 11 - m_Shift));
+
+			if (m_Exp)
+			{
+				if (!m_Decrease && m_Level > 0x6000)
+					cStep >>= 2;
+
+				if (m_Decrease)
+					step = static_cast<s16>((step * m_Level) >> 15);
+			}
+
+			m_Counter += cStep;
+
+			if (m_Counter >= 0x8000)
+			{
+				m_Counter = 0;
+				m_Level = std::clamp<s32>(m_Level + step, 0, INT16_MAX);
+			}
+		}
 
 	protected:
 		u8 m_Shift{0};
@@ -82,13 +112,94 @@ namespace SPU
 			Stopped,
 		};
 
-		void Run();
-		void Attack();
-		void Release();
-		void Stop();
+		void Run()
+		{
+			// Let's not waste time calculating silent voices
+			if (m_Phase == Phase::Stopped)
+				return;
+
+			Step();
+
+			if (m_Phase == Phase::Sustain)
+				return;
+
+			if ((!m_Decrease && m_Level >= m_Target) || (m_Decrease && m_Level <= m_Target))
+			{
+				switch (m_Phase)
+				{
+					case Phase::Attack:
+						m_Phase = Phase::Decay;
+						break;
+					case Phase::Decay:
+						m_Phase = Phase::Sustain;
+						break;
+					case Phase::Release:
+						m_Phase = Phase::Stopped;
+						break;
+					default:
+						break;
+				}
+
+				UpdateSettings();
+			}
+		}
+		void Attack()
+		{
+			m_Phase = Phase::Attack;
+			m_Level = 0;
+			m_Counter = 0;
+			UpdateSettings();
+		}
+		void Release()
+		{
+			m_Phase = Phase::Release;
+			m_Counter = 0;
+			UpdateSettings();
+		}
+		void Stop()
+		{
+			m_Phase = Phase::Stopped;
+			m_Level = 0;
+		}
 		[[nodiscard]] s16 Level() const { return static_cast<short>(m_Level); };
 		void SetLevel(s16 value) { m_Level = value; }
-		void UpdateSettings();
+		void UpdateSettings()
+		{
+			switch (m_Phase)
+			{
+				case Phase::Attack:
+					m_Exp = m_Reg.AttackExp;
+					m_Decrease = false;
+					m_Shift = m_Reg.AttackShift;
+					m_Step = static_cast<s8>(7 - m_Reg.AttackStep.GetValue());
+					m_Target = 0x7FFF;
+					break;
+				case Phase::Decay:
+					m_Exp = true;
+					m_Decrease = true;
+					m_Shift = m_Reg.DecayShift;
+					m_Step = -8;
+					m_Target = (m_Reg.SustainLevel.GetValue() + 1) << 11;
+					break;
+				case Phase::Sustain:
+					m_Exp = m_Reg.SustainExp;
+					m_Decrease = m_Reg.SustainDecr;
+					m_Shift = m_Reg.SustainShift;
+					m_Step = m_Decrease ? static_cast<s8>(-8 + m_Reg.SustainStep.GetValue()) :
+										  static_cast<s8>(7 - m_Reg.SustainStep.GetValue());
+					m_Target = 0; // unused for sustain
+					break;
+				case Phase::Release:
+					m_Exp = m_Reg.ReleaseExp;
+					m_Decrease = true;
+					m_Shift = m_Reg.ReleaseShift;
+					m_Step = -8;
+					m_Target = 0;
+					break;
+				default:
+					break;
+			}
+		}
 		ADSRReg m_Reg{0};
 		[[nodiscard]] Phase GetPhase() const { return m_Phase; }
 
@@ -113,8 +224,44 @@ namespace SPU
 	class Volume : Envelope
 	{
 	public:
-		void Run();
-		void Set(u16 volume);
+		void Run()
+		{
+			if (!m_Sweep.EnableSweep)
+				return;
+
+			Step();
+		}
+		void Set(u16 volume)
+		{
+
+			m_Sweep.bits = volume;
+
+			if (!m_Sweep.EnableSweep)
+			{
+				m_Level = static_cast<s16>(m_Sweep.bits << 1);
+				return;
+			}
+
+			m_Exp = m_Sweep.SweepExp;
+			m_Decrease = m_Sweep.SweepDecrease;
+			m_Shift = m_Sweep.SweepShift;
+			m_Step = m_Decrease ? static_cast<s8>(-8 + m_Sweep.SweepStep.GetValue()) :
+								  static_cast<s8>(7 - m_Sweep.SweepStep.GetValue());
+
+			if (m_Exp && m_Decrease)
+			{
+				if (m_Sweep.NegativePhase)
+					Console.WriteLn("Disqualified from inv");
+				m_Inv = false;
+			}
+			else
+			{
+				m_Inv = m_Sweep.NegativePhase;
+			}
+
+			Console.WriteLn(Color_Red, "start sweep, e:%d d:%d sh:%d st:%d inv:%d", m_Exp, m_Decrease, m_Shift, m_Step, m_Inv);
+			Console.WriteLn(Color_Red, "Current level %08x", m_Level);
+		}
 		[[nodiscard]] u16 Get() const { return m_Sweep.bits; }
 		[[nodiscard]] s16 GetCurrent() const { return static_cast<s16>(m_Level); }
 
