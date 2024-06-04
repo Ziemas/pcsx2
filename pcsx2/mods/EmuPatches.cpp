af1665976c8657c79d001a2f438c17c2d9fdef2b
Ziemas <ziemas@ziemas.se>
2024-06-04 23:54:37 +0200

    Fix

--- pcsx2/mods/EmuPatches.cpp
+++ pcsx2/mods/EmuPatches.cpp
@@ -6,7 +6,7 @@
 //This one won't be visible in UI
 void PTR2AspectRatioSet(AspectRatioType ar)
 {
-	char buf[4] = {};
+	u8 buf[4] = {};
 	switch (ar)
 	{
 		//adapted from Parotaku's widescreen patch
@@ -25,13 +25,13 @@ void PTR2AspectRatioSet(AspectRatioType ar)
 		
 			{
 				//patch=1,EE,0016066c,word,3c013f40
-				char buf2[4] = {0x40, 0x3F, 0x01, 0x3C};
+				u8 buf2[4] = {0x40, 0x3F, 0x01, 0x3C};
 				vtlb_memSafeWriteBytes(0x0016066c, &buf2, 4);
 				//patch=1,EE,00160678,word,44810000
-				char buf3[4] = {00, 00, 0x81, 0x44};
+				u8 buf3[4] = {00, 00, 0x81, 0x44};
 				vtlb_memSafeWriteBytes(0x00160678, &buf3, 4);
 				//patch=1,EE,0016067c,word,4600c602
-				char buf4[4] = {0x02, 0xC6, 0x00, 0x46};
+				u8 buf4[4] = {0x02, 0xC6, 0x00, 0x46};
 				vtlb_memSafeWriteBytes(0x0016067c, &buf4, 4);
 			}
 			break;
@@ -40,23 +40,23 @@ void PTR2AspectRatioSet(AspectRatioType ar)
 				u32 height = g_gs_device->GetWindowHeight();
 
 				//patch=1,EE,0016066c,word,3c013f40
-				char buf[4] = {0x40, 0x3F, 0x01, 0x3C};
+				u8 buf[4] = {0x40, 0x3F, 0x01, 0x3C};
 				vtlb_memSafeWriteBytes(0x0016066c, &buf, 4);
 
 				//replace the "3f40" (0.75 for 16:9) with our new multiplier
 				float heightReal = height * 1.071; //to get 448 to 480
 				heightReal = heightReal * 1.25; //this number has no meaning its eyeballed 
 				float multiplier = heightReal / float(width);
-				char buf2[4] = {};
+				u8 buf2[4] = {};
 				memcpy(buf2, &multiplier, 4);
 				vtlb_memSafeWriteBytes(0x0016066c, &buf2[2], 1);
 				vtlb_memSafeWriteBytes(0x0016066d, &buf2[3], 1);
 
 				//patch=1,EE,00160678,word,44810000
-				char buf3[4] = {00, 00, 0x81, 0x44};
+				u8 buf3[4] = {00, 00, 0x81, 0x44};
 				vtlb_memSafeWriteBytes(0x00160678, &buf3, 4);
 				//patch=1,EE,0016067c,word,4600c602
-				char buf4[4] = {0x02, 0xC6, 0x00, 0x46};
+				u8 buf4[4] = {0x02, 0xC6, 0x00, 0x46};
 				vtlb_memSafeWriteBytes(0x0016067c, &buf4, 4);
 		break;
 	}
